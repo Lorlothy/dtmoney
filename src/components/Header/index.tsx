626251df2710ff63f233c702b9171f23de20658f
Lorlothy <fael.rgt@gmail.com>
2021-05-23 20:40:10 -0300

    Created Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,21 @@
 import logo from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void
+}
 
-export function Header() {
+export function Header({onOpenNewTransactionModal}: HeaderProps) {
+    
+
     return(
-        <Container>
-            <Content>
-                <img src={logo} alt="dt money" />
-                <button type="button">Nova transação</button>
-            </Content>
-        </Container>
+    <Container>
+        <Content>
+            <img src={logo} alt="dt money" />
+            <button type="button" onClick={onOpenNewTransactionModal}>Nova transação</button>
+
+                
+        </Content>
+    </Container>
     )
 }
---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,31 @@
+import Modal from 'react-modal'
+import { Container } from './styles'
+
+interface NewTransactionModalProps {
+    isOpen:boolean
+    onRequestClose: () => void
+}
+
+export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {    
+
+    return (
+        <Modal 
+         isOpen={isOpen} 
+         onRequestClose={onRequestClose}
+       >
+        <Container>
+
+             <h2>Cadastrar Transação</h2>
+
+             <input placeholder="Titulo"/>
+
+             <input 
+                type="number"
+                placeholder="Valor"
+             />
+        </Container>
+
+
+       </Modal>
+    )
+}
