4e6f5bcdb6126ef38a28eda6f96605117e0e32f9
Lorlothy <fael.rgt@gmail.com>
2021-05-30 17:39:38 -0300

    Modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,11 @@
 import Modal from 'react-modal'
-import { Container } from './styles'
+import closeImg from '../../assets/close.svg'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+
+
+
+import { Container,TransactionTypeContainer } from './styles'
 
 interface NewTransactionModalProps {
     isOpen:boolean
@@ -12,17 +18,54 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
         <Modal 
          isOpen={isOpen} 
          onRequestClose={onRequestClose}
+         overlayClassName="react-modal-overlay"
+         className="react-modal-content"
+
        >
+           <button 
+            type="button" 
+            onClick={onRequestClose} 
+            className="react-modal-close"
+           >
+               <img src={closeImg} alt="Fechar"/>
+           </button>
         <Container>
 
              <h2>Cadastrar Transação</h2>
 
              <input placeholder="Titulo"/>
 
-             <input 
+            <input 
                 type="number"
                 placeholder="Valor"
-             />
+            />
+
+            <TransactionTypeContainer>
+                <button
+                 type="button"
+                >
+                    <img src={incomeImg} alt="Entrada"></img>
+                    <span>Entrada</span>
+
+                </button>
+
+                <button
+                 type="button"
+                >
+                    <img src={outcomeImg} alt="Saida"></img>
+                    <span>Saida</span>
+
+                </button>
+            </TransactionTypeContainer>
+
+            <input                
+                placeholder="Categoria"
+            />
+
+            <button type="submit">
+                Cadastrar
+            </button>
+
         </Container>
 
 
